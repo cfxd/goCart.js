1d211858ae375bfa9787e1f3201eb41bf13ca168
Michael Silber <michael@cfxdesign.com>
2020-12-08 12:52:28 -0500

    Update all cart counts

--- src/lib/GoCart.js
+++ src/lib/GoCart.js
@@ -63,7 +63,7 @@ class GoCart {
         this.cartMiniCartFooter = document.querySelector(this.defaults.cartMiniCartFooter);
         this.cartTrigger = document.querySelectorAll(this.defaults.cartTrigger);
         this.cartOverlay = document.querySelector(this.defaults.cartOverlay);
-        this.cartCount = document.querySelector(this.defaults.cartCount);
+        this.cartCount = document.querySelectorAll(this.defaults.cartCount);
         this.addToCart = document.querySelectorAll(this.defaults.addToCart);
         this.removeFromCart = this.defaults.removeFromCart;
         this.removeFromCartNoDot = this.defaults.removeFromCartNoDot;
@@ -235,7 +235,9 @@ class GoCart {
     }
 
     cartItemCount(cart) {
-        this.cartCount.innerHTML = cart.item_count;
+        this.cartCount.forEach((item, index) => {
+            item.innerHTML = cart.item_count;
+        });
     }
 
     fetchAndOpenCart() {
